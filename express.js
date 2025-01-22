5f02a82cd7454e6cab19be22e5e6e9730700fdaf
Onya-Code <andyonyaka@gmail.com>
2025-01-22 07:11:15 +0100

    Update express.js

--- express.js
+++ express.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const routes = require('../Routes/auth.routes');
-const routes = require('../Routes/post.routes');
+//const routes = require('../Routes/auth.routes');
+//const routes = require('../Routes/post.routes');
 
 const app = express();
 
