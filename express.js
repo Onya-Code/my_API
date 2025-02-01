69f4bff9e417fb0560c904dfd65bed97064c43a7
Onya-Code <andyonyaka@gmail.com>
2025-02-01 11:42:08 +0100

    Update express.js

--- express.js
+++ express.js
@@ -1,10 +1,15 @@
 const express = require("express");
-//const routes = require('../Routes/auth.routes');
-//const routes = require('../Routes/post.routes');
+const routes = require('../Routes/auth.routes');
+const routes = require('../Routes/post.routes');
 
 const app = express();
 
 const PORT = process.env.PORT || 4004
+
+app.use(express.json());
+
+app.use('/auth', AuthRoutes);
+app.use('/post', PostRoutes);
 
 app.get('/', (req, res) => {
  res.send('Welcome to our api blog');
